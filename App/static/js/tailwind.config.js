a54e2448a632cad08bfc151c2847be4a8387784a
jean de dieu <jeandedieusessou@gmail.com>
2024-12-11 19:19:28 +0000

    style : implement header element 	add custom color in tailwind config file

---
+++ App/static/js/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        text: "#032508",
+        background: "#edfdef",
+        primary: "#5eaa65",
+        secondary: "#a4d5a9",
+        accent: "#75c87c",
+      },
+    },
+  },
+};
